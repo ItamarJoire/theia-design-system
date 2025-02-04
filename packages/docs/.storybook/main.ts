6ba8d8e6132db805ab8afe76b054f871a29352d8
Itamar Joire <ijoire7@gmail.com>
2025-02-04 16:11:54 -0300

    fix: add subpath

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,9 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite'
-import { join, dirname } from 'path'
-
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
-}
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
